37e058d604febadc73275aaf0b4dbc4a0ab7e96c
Fawaz Farid <fawwazally@gmail.com>
2023-03-15 20:00:26 +0300

    Update initializer

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,5 +1,3 @@
 require 'slack-notifier'
 
-SLACK_NOTIFIER = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL'] do
-  defaults channel: ENV['SLACK_CHANNEL'], username: ENV['SLACK_USERNAME']
-end
+SLACK_NOTIFIER = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
